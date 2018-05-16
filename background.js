dac3c66401c5292451c11a4cc1d530f7eebaa052
hpat0003 <hpat0003@student.monash.edu>
2018-05-17 05:03:11 +1000

    can download folders

--- background.js
+++ background.js
@@ -1,10 +1,18 @@
 chrome.browserAction.onClicked.addListener(function(tab){chrome.tabs.executeScript({file: "download.js"}, function(result){
-	for (i = 0; i < result[0].length; i++) {
-		const resource = result[0][i]
+	console.log(result);
+	
+	for (i = 0; i < result[0][0].length; i++) {
+		const resource = result[0][0][i]
 		chrome.downloads.download({url: resource[0], filename: resource[1]})
+	}
+	
+	for (i = 0; i < result[0][1].length; i++) {
+		console.log("opening ", result[0][1][i]);
+		chrome.tabs.create({ url: result[0][1][i] });
 	}
 	})});
 
 //chrome.browserAction.onClicked.addListener(function(tab){chrome.tabs.executeScript({file: "download.js"}, function(result){
-//	console.log(result)
+//	console.log(result[0]);
+//	console.log(result[1]);
 //	})});
--- download.js
+++ download.js
@@ -1,4 +1,4 @@
-const resourceContainer = document.getElementsByClassName("activityinstance");
+var resourceContainer = document.getElementsByClassName("activityinstance");
 var resources = [];
 
 for (i = 0; i < resourceContainer.length; i++) {
@@ -6,10 +6,27 @@ for (i = 0; i < resourceContainer.length; i++) {
 	if (name.slice(-4) != "File") {
 		continue
 	}
-	name = name.replace(/:/g , " -");
-	const fullname = name.slice(0,-5) + ".pdf"
-	const url = resourceContainer[i].querySelector("a").href + "&redirect=1";
+	name = name.slice(0,-5).replace(/:/g , " -");
+	if (resourceContainer[i].querySelector("img").src.slice(-7,-3) == "text") {
+		fullname = name + ".txt"
+	} else {
+		fullname = name + ".pdf";
+	}
+	var url = resourceContainer[i].querySelector("a").href + "&redirect=1";
 	resources.push([url, fullname]);
 }
-console.log(resources);
-resources;
+//console.log(resources);
+
+
+var foldersContainers = document.getElementsByClassName("box generalbox folderbuttons");
+var folders = [];
+for (i = 0; i < foldersContainers.length; i++) {
+	var folderId = foldersContainers[i].querySelectorAll("input")[1].value;
+	var url = "https://moodle.vle.monash.edu/mod/folder/download_folder.php?id=" + folderId;
+	folders.push(url)
+}
+//console.log(folders);
+
+result = [resources, folders];
+//console.log(result);
+result;
