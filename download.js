3fd8a08aadbd44aa0c2842ece737a91838624161
hpat0003 <hpat0003@student.monash.edu>
2018-05-20 21:44:26 +1000

    handling the errors

--- download.js
+++ download.js
@@ -2,18 +2,15 @@ var resourceContainer = document.getElementsByClassName("activityinstance");
 var resources = [];
 
 for (i = 0; i < resourceContainer.length; i++) {
-	var name = resourceContainer[i].getElementsByClassName("instancename")[0].innerText;
-	if (name.slice(-4) != "File") {
-		continue
+	try {
+		var name = resourceContainer[i].getElementsByClassName("instancename")[0].innerText;
+		if (name.slice(-4) == "File") {
+			var url = resourceContainer[i].querySelector("a").href + "&redirect=1";
+			resources.push([url]);
+		}	
+	} catch (error) {
+		console.log(i, error)
 	}
-	name = name.slice(0,-5).replace(/:/g , " -");
-	if (resourceContainer[i].querySelector("img").src.slice(-7,-3) == "text") {
-		fullname = name + ".txt"
-	} else {
-		fullname = name + ".pdf";
-	}
-	var url = resourceContainer[i].querySelector("a").href + "&redirect=1";
-	resources.push([url, fullname]);
 }
 //console.log(resources);
 
