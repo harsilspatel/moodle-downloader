1ac9038911713c7588e52d710bbf8c5243165879
harsilspatel <harsilspatel@gmail.com>
2018-06-24 19:49:27 +1000

    prettified the code, prettier.io ✨

--- background.js
+++ background.js
@@ -1,21 +1,23 @@
-chrome.browserAction.onClicked.addListener(function(tab){chrome.tabs.executeScript({file: "download.js"}, function(result){
-//	console.log(result);
-	function downloadFiles(result) {
-		for (i = 0; i < result[0][0].length; i++) {
-				const resource = result[0][0][i]
-				chrome.downloads.download({url: resource[0]})
-		}	
-	}
-	
-	function downloadFolders(result) {
-	for (i = 0; i < result[0][1].length; i++) {
-			console.log("opening ", result[0][1][i]);
-			chrome.tabs.create({ url: result[0][1][i] });
-		}	
-	}
-	downloadFiles(result);
-	downloadFolders(result);
-	})});
+chrome.browserAction.onClicked.addListener(function(tab) {
+	chrome.tabs.executeScript({ file: "download.js" }, function(result) {
+		//	console.log(result);
+		function downloadFiles(result) {
+			for (i = 0; i < result[0][0].length; i++) {
+				const resource = result[0][0][i];
+				chrome.downloads.download({ url: resource[0] });
+			}
+		}
+
+		function downloadFolders(result) {
+			for (i = 0; i < result[0][1].length; i++) {
+				console.log("opening ", result[0][1][i]);
+				chrome.tabs.create({ url: result[0][1][i] });
+			}
+		}
+		downloadFiles(result);
+		downloadFolders(result);
+	});
+});
 
 //chrome.browserAction.onClicked.addListener(function(tab){chrome.tabs.executeScript({file: "download.js"}, function(result){
 //	console.log(result[0]);
--- download.js
+++ download.js
@@ -2,11 +2,15 @@ function getFiles(resourceContainers) {
 	var files = [];
 	for (i = 0; i < resourceContainers.length; i++) {
 		try {
-			var name = resourceContainers[i].getElementsByClassName("instancename")[0].innerText;
+			var name = resourceContainers[i].getElementsByClassName(
+				"instancename"
+			)[0].innerText;
 			if (name.slice(-4) == "File") {
-				var url = resourceContainers[i].querySelector("a").href + "&redirect=1";
+				var url =
+					resourceContainers[i].querySelector("a").href +
+					"&redirect=1";
 				files.push([url]);
-			}	
+			}
 		} catch (error) {
 			console.log(i, error);
 		}
@@ -19,11 +23,14 @@ function getFolders() {
 	var foldersContainers = document.getElementsByClassName("singlebutton");
 	var folders = [];
 	for (i = 0; i < foldersContainers.length; i++) {
-		var url = foldersContainers[i].querySelector("form").action + "?id=" + foldersContainers[i].querySelectorAll("input")[1].value
-		folders.push(url)
+		var url =
+			foldersContainers[i].querySelector("form").action +
+			"?id=" +
+			foldersContainers[i].querySelectorAll("input")[1].value;
+		folders.push(url);
 	}
 	//console.log(folders);
-	return folders
+	return folders;
 }
 //console.log(result);
 var resourceContainers = document.getElementsByClassName("activityinstance");
