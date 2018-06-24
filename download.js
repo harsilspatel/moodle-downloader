9a0dc4f018190e82099f63f45bb9721a96f36423
harsilspatel <harsilspatel@gmail.com>
2018-06-24 19:43:17 +1000

    refactored code into functions

--- download.js
+++ download.js
@@ -1,29 +1,31 @@
-var resourceContainer = document.getElementsByClassName("activityinstance");
-var resources = [];
-
-for (i = 0; i < resourceContainer.length; i++) {
-	try {
-		var name = resourceContainer[i].getElementsByClassName("instancename")[0].innerText;
-		if (name.slice(-4) == "File") {
-			var url = resourceContainer[i].querySelector("a").href + "&redirect=1";
-			resources.push([url]);
-		}	
-	} catch (error) {
-		console.log(i, error)
+function getFiles(resourceContainers) {
+	var files = [];
+	for (i = 0; i < resourceContainers.length; i++) {
+		try {
+			var name = resourceContainers[i].getElementsByClassName("instancename")[0].innerText;
+			if (name.slice(-4) == "File") {
+				var url = resourceContainers[i].querySelector("a").href + "&redirect=1";
+				files.push([url]);
+			}	
+		} catch (error) {
+			console.log(i, error);
+		}
 	}
+	//console.log(resources);
+	return files;
 }
-//console.log(resources);
 
-
-var foldersContainers = document.getElementsByClassName("singlebutton");
-var folders = [];
-for (i = 0; i < foldersContainers.length; i++) {
-	var url = foldersContainers[i].querySelector("form").action + "?id=" + foldersContainers[i].querySelectorAll("input")[1].value
-
-	folders.push(url)
+function getFolders() {
+	var foldersContainers = document.getElementsByClassName("singlebutton");
+	var folders = [];
+	for (i = 0; i < foldersContainers.length; i++) {
+		var url = foldersContainers[i].querySelector("form").action + "?id=" + foldersContainers[i].querySelectorAll("input")[1].value
+		folders.push(url)
+	}
+	//console.log(folders);
+	return folders
 }
-//console.log(folders);
-
-result = [resources, folders];
 //console.log(result);
+var resourceContainers = document.getElementsByClassName("activityinstance");
+result = [getFiles(resourceContainers), getFolders()];
 result;
