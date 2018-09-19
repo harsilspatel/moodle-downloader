7a4893b290367d8f7152d6e7d5c711e048ed35ac
hpat0003 <hpat0003@student.monash.edu>
2018-09-19 20:04:24 +1000

    added support for Resources tab

--- background.js
+++ background.js
@@ -1,17 +1,20 @@
 function getFiles() {
-	console.log('inside getFiles')
 	let resourceContainers = Array.from(document.getElementsByClassName("activityinstance")); // realised this is not beautifulSoup 😆
 	return resourceContainers
 		.filter(resource => (	// filtering out just the files. Noob filtering going on here 😝
 			resource.getElementsByClassName("instancename")[0].innerText.slice(-4) == "File"))
 		.map(resource => ({
 			name: resource.getElementsByClassName("instancename")[0].innerText.slice(0, -4),
-			url: resource.querySelector("a").href + "&redirect=1"
-		}));
+			url: resource.getElementsByTagName("a").href + "&redirect=1"}))
+		.concat(Array.from(document.getElementsByClassName('cell c1'))
+			.filter(resource => resource.getElementsByTagName('img')[0]['alt'] == "File")
+			.map(resource => ({
+				name: resource.getElementsByTagName('a')[0].innerText,
+				url: resource.getElementsByTagName('a')[0].href + "&redirect=1"
+			})))
 }
 
 function getFolders() {
-	console.log('inside getFolders')
 	var foldersContainers = document.getElementsByClassName("singlebutton");
 	for (i = 0; i < foldersContainers.length; i++) {
 		var url =
@@ -20,7 +23,6 @@ function getFolders() {
 			foldersContainers[i].querySelectorAll("input")[1].value;
 		folders.push(url);
 	}
-	//console.log(folders);
 	return folders;
 }
 
