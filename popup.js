1861a9c7508496e299b23145e817bf8e455623e8
hpat0003 <hpat0003@student.monash.edu>
2018-10-03 14:43:20 +1000

    migrated to new analytics

--- popup.js
+++ popup.js
@@ -1,15 +1,24 @@
 function main() {
 
 	// google analytics
-	var _gaq = _gaq || [];
-	_gaq.push(['_setAccount', 'UA-119398707-1']);
-	_gaq.push(['_trackPageview']);
 
-	(function() {
-		var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-		ga.src = 'https://ssl.google-analytics.com/ga.js';
-		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-	})();
+	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+		ga('create', 'UA-119398707-1', 'auto');  // Replace with your property ID.
+		ga('send', 'pageview');
+
+	// var _gaq = _gaq || [];
+	// _gaq.push(['_setAccount', 'UA-119398707-1']);
+	// _gaq.push(['_trackPageview']);
+
+	// (function() {
+	// 	var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+	// 	ga.src = 'https://ssl.google-analytics.com/ga.js';
+	// 	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+	// })();
 
 	// downloadStuff on button press
 	let button = document.getElementById("downloadStuff");
@@ -56,7 +65,8 @@ function filterList() {
 function downloadStuff() {
 	let resourceSelector = document.getElementById("resourceSelector");
 	Array.from(resourceSelector.options).filter(option => option.selected).forEach(option => chrome.downloads.download({url: option.value}));
-	_gaq.push(['_trackEvent', 'downloadStuff', 'clicked']);
+	// _gaq.push(['_trackEvent', 'downloadStuff', 'clicked']);
+	ga('send', 'downloadStuff', 'func', 'clicked');
 }
 
 document.addEventListener('DOMContentLoaded', function() {
