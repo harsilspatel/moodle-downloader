b3d382b633ba3b01df42729bbc569e75ae4b8c1a
Michael <mcpower@users.noreply.github.com>
2018-12-31 23:32:32 +1100

    Add ability to download folders

--- src/background.js
+++ src/background.js
@@ -4,17 +4,59 @@
  * https://github.com/harsilspatel/MoodleDownloader
  */
 
+function getActivityName(activity) {
+	return activity.getElementsByClassName("instancename")[0].firstChild.textContent.trim();
+}
+
 function getFilesUnderSection() {
+	// The session key should normally be accessible through window.M.cfg.sesskey,
+	// but getting the window object is hard.
+	// Instead, we can grab the session key from the logout button:
+	const logoutButton = document.querySelector("a[href*='login/logout.php']");
+	const sesskey = (new URL(logoutButton.href)).searchParams.get("sesskey");
 	return Array.from(document.getElementsByClassName('content'))
-		.map(content =>
-			Array.from(content.getElementsByClassName("activity resource modtype_resource "))
+		.map(content => {
+			const sectionEl = content.querySelector("h3.sectionname");
+			if (!sectionEl)
+				return [];
+			const section = sectionEl.textContent.trim();
+			const resources = Array.from(content.getElementsByClassName("activity resource modtype_resource "))
 				.map(resource => ({
-					name: resource.getElementsByClassName("instancename")[0].firstChild.textContent.trim(),
+					name: getActivityName(resource),
 					downloadOptions: {
 						url: resource.getElementsByTagName("a")[0].href + "&redirect=1"
 					},
-					section: content.getElementsByTagName("h3")[0].textContent.trim()})))
-		.reduce((x, y) => x.concat(y), []);
+					section: section}));
+			const folders = Array.from(content.getElementsByClassName("activity folder modtype_folder "))
+				.map(folder => {
+					const url = new URL(folder.getElementsByTagName("a")[0].href);
+					const id = url.searchParams.get("id");
+					// We will modify the downloadURL such that each folder has a
+					// unique download URL (so suggestFilename will work).
+					// Adding "?id=ID" to the POST URL still results in a valid
+					// request, so we can use this to uniquely identify downloads.
+					const downloadUrl =
+						url.origin +
+						url.pathname.slice(undefined, url.pathname.lastIndexOf("/")) +
+						"/download_folder.php?id=" + id;
+					return {
+						name: getActivityName(folder),
+						downloadOptions: {
+							url: downloadUrl,
+							method: "POST",
+							headers: [
+								{
+									name: "content-type",
+									value: "application/x-www-form-urlencoded"
+								}
+							],
+							body: `id=${id}&sesskey=${sesskey}`
+						},
+						section: section
+					};
+				});
+			return resources.concat(folders);
+		}).reduce((x, y) => x.concat(y), []);
 }
 
 function getFilesUnderResources() {
