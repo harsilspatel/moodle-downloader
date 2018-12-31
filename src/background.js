40168cd4a5dc33263c0970af4fd27f17edda67a0
Michael <mcpower@users.noreply.github.com>
2019-01-01 00:27:01 +1100

    Support folders under resources
    getFilesUnderSection is significantly cleaner, too!

--- src/background.js
+++ src/background.js
@@ -4,58 +4,59 @@
  * https://github.com/harsilspatel/MoodleDownloader
  */
 
+// The session key should normally be accessible through window.M.cfg.sesskey,
+// but getting the window object is hard.
+// Instead, we can grab the session key from the logout button:
+const sesskey = (
+	new URL(document.querySelector("a[href*='login/logout.php']").href)
+).searchParams.get("sesskey");
+
+function getDownloadOptions(url) {
+	if (!url.includes("folder")) {
+		// Resources and other unsupported types.
+		return {
+			url: url + "&redirect=1"
+		};
+	}
+	const urlObj = new URL(url);
+	const id = urlObj.searchParams.get("id");
+	// We will modify the downloadURL such that each folder has a
+	// unique download URL (so suggestFilename will work).
+	// Adding "?id=ID" to the POST URL still results in a valid
+	// request, so we can use this to uniquely identify downloads.
+	const downloadUrl =
+		urlObj.origin +
+		urlObj.pathname.slice(undefined, urlObj.pathname.lastIndexOf("/")) +
+		"/download_folder.php?id=" + id;
+	return {
+		url: downloadUrl,
+		method: "POST",
+		headers: [
+			{
+				name: "content-type",
+				value: "application/x-www-form-urlencoded"
+			}
+		],
+		body: `id=${id}&sesskey=${sesskey}`
+	};
+}
+
 function getActivityName(activity) {
 	return activity.getElementsByClassName("instancename")[0].firstChild.textContent.trim();
 }
 
 function getFilesUnderSection() {
-	// The session key should normally be accessible through window.M.cfg.sesskey,
-	// but getting the window object is hard.
-	// Instead, we can grab the session key from the logout button:
-	const logoutButton = document.querySelector("a[href*='login/logout.php']");
-	const sesskey = (new URL(logoutButton.href)).searchParams.get("sesskey");
 	return Array.from(document.getElementsByClassName('content'))
 		.map(content => {
 			const sectionEl = content.querySelector("h3.sectionname");
 			if (!sectionEl)
 				return [];
 			const section = sectionEl.textContent.trim();
-			const resources = Array.from(content.getElementsByClassName("activity resource modtype_resource "))
-				.map(resource => ({
-					name: getActivityName(resource),
-					downloadOptions: {
-						url: resource.getElementsByTagName("a")[0].href + "&redirect=1"
-					},
+			return Array.from(content.querySelectorAll("activity resource, activity folder"))
+				.map(activity => ({
+					name: getActivityName(activity),
+					downloadOptions: getDownloadOptions(activity.getElementsByTagName("a")[0].href),
 					section: section}));
-			const folders = Array.from(content.getElementsByClassName("activity folder modtype_folder "))
-				.map(folder => {
-					const url = new URL(folder.getElementsByTagName("a")[0].href);
-					const id = url.searchParams.get("id");
-					// We will modify the downloadURL such that each folder has a
-					// unique download URL (so suggestFilename will work).
-					// Adding "?id=ID" to the POST URL still results in a valid
-					// request, so we can use this to uniquely identify downloads.
-					const downloadUrl =
-						url.origin +
-						url.pathname.slice(undefined, url.pathname.lastIndexOf("/")) +
-						"/download_folder.php?id=" + id;
-					return {
-						name: getActivityName(folder),
-						downloadOptions: {
-							url: downloadUrl,
-							method: "POST",
-							headers: [
-								{
-									name: "content-type",
-									value: "application/x-www-form-urlencoded"
-								}
-							],
-							body: `id=${id}&sesskey=${sesskey}`
-						},
-						section: section
-					};
-				});
-			return resources.concat(folders);
 		}).reduce((x, y) => x.concat(y), []);
 }
 
@@ -64,15 +65,13 @@ function getFilesUnderResources() {
 			.filter(resource => resource.getElementsByTagName('img').length != 0)
 			.map(resource => (resource = {
 				name: resource.getElementsByTagName('a')[0].textContent.trim(),
-				downloadOptions: {
-					url: resource.getElementsByTagName('a')[0].href + "&redirect=1"
-				},
+				downloadOptions: getDownloadOptions(resource.getElementsByTagName('a')[0].href),
 				type: resource.getElementsByTagName('img')[0]['alt'],
 				section: resource.getElementsByTagName('td')[0].textContent.trim()}))
 			.map((resource, index, array) => {
 				resource.section = (resource.section ? resource.section : array[index-1].section);
 				return resource})
-			.filter(resource => resource.type == 'File')
+			.filter(resource => resource.type === 'File' || resource.type === 'Folder')
 }
 
 function getFiles() {
